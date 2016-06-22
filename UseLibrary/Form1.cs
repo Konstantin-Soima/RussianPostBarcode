5426234578f8ef2d27136d0322b1deb76ed7b6ed
Konstantin Soyma <soyma-k@mail.ru>
2016-06-22 08:04:28 +0300

    Publishing

---
+++ UseLibrary/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RBBarcode;
+
+namespace UseLibrary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image =  PostBarcode.Print2of5Interleaved(textBox1.Text);//14211716007389
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Image = PostBarcode.PrintPostcode(textBox2.Text);
+        }
+    }
+}
---
+++ WebBarcode/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RBBarcode;
+using System.Drawing;
+using System.IO;
+
+namespace WebBarcode.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Barcode
+        public ActionResult Barcode(string num)
+        {
+            Bitmap qrCodeImage = PostBarcode.Print2of5Interleaved(num);
+         
+            MemoryStream ms = new MemoryStream();
+            qrCodeImage.Save(ms,System.Drawing.Imaging.ImageFormat.Gif);
+            ms.Position = 0;
+            return File(ms, "image/gif");
+        }
+        // GET: Barcode
+        public ActionResult Postcode(string index)
+        {
+            Bitmap postCode = PostBarcode.PrintPostcode(index);
+            MemoryStream ms = new MemoryStream();
+            postCode.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            ms.Position = 0;
+            return File(ms, "image/gif");
+        }
+    }
+
+
+}
